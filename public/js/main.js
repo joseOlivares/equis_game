bbe8302362332eeccff0df749e9cf8a202796868
José Luis Olivares <joseluis_503@yahoo.com>
2018-10-10 17:26:00 -0600

    More changes

--- public/js/main.js
+++ public/js/main.js
@@ -6,12 +6,30 @@ var app = {
     },
 
     listenSocket:function(){
-         var socket = io.connect(this.serverUrl); //creating socket connection
-
+        var socket = io.connect(this.serverUrl); //creating socket connection
+        var userName=-1;
         socket.on('users connected', function(data){
-            $('#usersConnected').html(data); //displaying how many connections are.
+            $('#usersConnected').html(data); //mostrando usuarios conectados
         }); 
-    
-    }
+
+
+        $('#btnLogin').on('click',function(){
+                userName=$('#txtUserName').val();//Nombre de usuario
+                $('#txtUserName').prop('disabled', true); //Desabilitando el input
+                $('#btnLogin').prop('disabled',true);//disabling btnLogin
+                
+
+                /*$('#idsender').prop('disabled', true); //disaabling input idsender
+                $('#m').prop('disabled', false); //enabling input idsender
+                socket.emit('user logged',currentUserId); //enviando ID de usurio logead    
+                toReceivers(currentUserId);//adding posible receiver to html select, calling a function 
+                $('#btnLogin').prop('disabled',true);//disabling btnLogin
+                $('#btnLogout').prop('disabled',false);//enabling btnLogout
+                */
+        });        
+
+    },
+
+
 
 };
