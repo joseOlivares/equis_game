b751a6f6d4c1e3d67868e73a102b1bcff49ac934
José Luis Olivares <joseluis_503@yahoo.com>
2018-10-12 11:17:00 -0600

    saving user names

--- index.js
+++ index.js
@@ -3,23 +3,13 @@ var app = express();
 var server=require('http').createServer(app);
 var io = require('socket.io')(server);
 
-
-/*var express= require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-*/
-
 app.use(express.static(__dirname + '/public')); //serving statics files like css, js, images
-
 var port=process.env.PORT || 3000; //this is for heroku
-
-
 
 
 // Define/initialize our global vars
  var socketCount=0;
-//var connectedUsers = []; //array to store usernames and socket.id
+ var connectedUsers = []; //array to store usernames and socket.id
 //-------------------------------
 
 
@@ -30,17 +20,16 @@ app.get('/', function(req, res){
 //io.set('origins','*:*');
 
 io.on('connection', function(socket){
-	socketCount++;// Socket has connected, increase socket count
-	io.sockets.emit('users connected', socketCount);    // Let all sockets know how many are connected
-
 	socket.on('user loging',function(userName){
+		socketCount++;// Socket has connected, increase socket count
+		io.sockets.emit('users connected', socketCount);    // Let all sockets know how many are connected
 		
-		/*connectedUsers.push({
+		connectedUsers.push({
 			id : socket.id,
 			userName : userName
-		});*/
+		});
 
-		console.log('An user connected...'+userName.toString());
+		console.log('An user connected...'+connectedUsers[0].userName.toString());
 		//console.log(connectedUsers.toString());
 	});
 
