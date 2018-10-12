3cf0f62eb5d3ba57726e28b4985590c13aa28b51
José Luis Olivares <joseluis_503@yahoo.com>
2018-10-12 10:58:51 -0600

    Update index.js
    cabeceras middleware de index cambiadas

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var express= require('express');
+var app=require('express')();
+var http=require('http').Server(app);
+var io=require('socket.io')(http);
+/*var express= require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+*/
 
 app.use(express.static(__dirname + '/public')); //serving statics files like css, js, images
 
