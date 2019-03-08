5262df683396cfc113a404c67600b0de6ed6ab03
José Luis Olivares <joseluis_503@yahoo.com>
2019-03-08 10:55:29 -0600

    Error fixed

--- public/js/main.js
+++ public/js/main.js
@@ -15,7 +15,7 @@ var app = {
         app.listenSocket();
         app.prepareBoard(true, true); //prepareBoard(Does it put happy faces?, are the buttons disabled?)
         //mostrando indicaciones conuikit notification
-        UIkit.notification({message: 'Write your name <span uk-icon=\'icon: arrow-right\'></span>'});
+        
     };
     listenSocket:function(){
         //var socket = io.connect(this.serverUrl); //creating socket connection
