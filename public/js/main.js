6eade319659576302e58461ee0dfe4fdc7e43d88
José Luis Olivares <joseluis_503@yahoo.com>
2019-03-13 11:21:15 -0600

    Fire tooltip after element is showed

--- public/js/main.js
+++ public/js/main.js
@@ -16,11 +16,12 @@ var app = {
         app.prepareBoard(true, true); //prepareBoard(Does it put happy faces?, are the buttons disabled?)
 
         //mostrando indicaciones conuikit notification
-        UIkit.notification({message: 'Write your name<span uk-icon=\'icon: arrow-right\'></span>',
-        status:'primary',
-        pos: 'top-left',
-        timeout:5000});
-        
+          UIkit.notification({message: 'Write your name<span uk-icon=\'icon: arrow-right\'></span>',
+          status:'primary',
+          pos: 'top-left',
+          timeout:5000});
+
+          UIkit.tooltip('txtUserName').show();    
     },
     listenSocket:function(){
         //var socket = io.connect(this.serverUrl); //creating socket connection
